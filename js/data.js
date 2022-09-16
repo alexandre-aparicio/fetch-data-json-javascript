7399c5ab79d292946a0b1ddf5f0bd8965aa358ef
alexandre-aparicio <83748607+alexandre-aparicio@users.noreply.github.com>
2022-09-16 17:22:44 +0200

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,27 @@
+{
+    "status": true,
+    "message": "valid response",
+    "more": [
+        {
+            "idCard": "1",
+            "nombre": "Behance",
+            "descripcion": "Behance es una red de sitios y servicios especializada en la autopromoción, incluyendo consultoría y sitios de portafolio en línea. Behance provee a una red de sitios llamados el «Served sites».",
+            "enlace": "https://www.behance.net/",
+            "icono": "fa-behance"
+        },
+        {
+            "idCard": "2",
+            "nombre": "Twitter",
+            "descripcion": "Twitter es un servicio de microblogueo, con sede en San Francisco, California, Estados Unidos.La red permite enviar mensajes de texto plano de corta longitud, con un máximo de 280 caracteres",
+            "enlace": "https://twitter.com",
+            "icono": "fa-twitter"
+        },
+        {
+            "idCard": "3",
+            "nombre": "Facebook",
+            "descripcion": "Facebook es un servicio de redes y medios sociales en línea estadounidense con sede en Menlo Park, California. Su sitio web fue lanzado el 4 de febrero de 2004 por Mark Zuckerberg.",
+            "enlace": "https://es-la.facebook.com",
+            "icono": "fa-facebook"
+        }
+    ]
+}
---
+++ js/data_des.js
@@ -0,0 +1,30 @@
+{
+    "status": true,
+    "message": "valid response",
+    "more": [
+        {
+            "idDest": "1",
+            "idCard": "1",
+            "cuenta": "Grupo de La cocina de Los Pueblos",
+            
+            "enlace": "https://es-la.facebook.com/groups/1569446093133873/",
+            "imagen": "https://img.freepik.com/vector-premium/logotipo-restaurante-cocina-insignia-emblema-hierro-fundido-sarten-retro-vintage_445285-412.jpg"
+        },
+        {
+            "idDest": "2",
+            "idCard": "3",
+            "cuenta": "Recetas De Cocina y Postres",
+            
+            "enlace": "https://www.facebook.com/groups/873786709468565",
+            "imagen": "https://img.freepik.com/vector-premium/sarten-hierro-fundido-logotipo-cocina-restaurante_445285-407.jpg"
+        },
+        {
+            "idDest": "3",
+            "idCard": "3",
+            "cuenta": "Celiaquía y cocina sin gluten",
+            
+            "enlace": "https://www.facebook.com/groups/873786709468565",
+            "imagen": "https://img.freepik.com/vector-premium/vintage-retro-rustico-antiguo-sarten-hierro-fundido-fuego-logotipo-cocina-restaurante-clasico_445285-404.jpg"
+        }
+    ]
+}
